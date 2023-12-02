0e8c02aedb75579ab99d7e41ca0ec5e287ba4fe4
Jey the Count <58235578+JakubOwadowski@users.noreply.github.com>
2023-12-02 15:58:29 +0100

    an attempt was made

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,9 @@
 import { generalLayoutMock } from "./mock";
 import Image from "next/image";
+import Social from "../social/Social"
 
 export default function Footer() {
-  return (
-    <footer className="bg-neutral-800 p-8">
+  return <footer className="bg-neutral-800 p-8">
       <div className="">
           {generalLayoutMock.map((item) => {
             return item.key === "aboutUsTitle" ?
@@ -23,14 +23,15 @@ export default function Footer() {
           })}
       </div>
       <div className="text-white font-bold py-8">
-        HERE BE NAV
+        <div>HERE BE NAV</div>
+        <Social/>
       </div>
       <hr className="h-px my-8 bg-neutral-500 border-0"/>
-      <div className="flex flex-col md:flex-row lg:flex-row justify-between max-h-32 items-start">
+      <div className="flex flex-col items-center md:flex-row md:items-start justify-between">
         <Image src="/images/logo-light-115x34.png" alt="Logo" width={115} height={10} />
         {generalLayoutMock.map((item) => {
             return item.key === "copyrightText" ?
-            <span className="text-neutral-500 mt-6">
+            <span className="text-neutral-500 mt-6 md:mt-0">
                 {item.value}
             </span>
             :
@@ -38,5 +39,4 @@ export default function Footer() {
           })}
       </div>
     </footer>
-  );
 }
---
+++ src/components/social/Social.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Social() {
+    return <div className="flex flex-row justify-around">
+        <Image src="/facebook.svg" alt="facebook" width={25} height={25} />
+        <Image src="/twitter.svg" alt="twitter" width={25} height={25} />
+        <Image src="/instagram.svg" alt="instagram" width={25} height={25} />
+        <Image src="/google.svg" alt="google" width={25} height={25} />
+        <Image src="/linkedin.svg" alt="linkedin" width={25} height={25} />
+    </div>
+}
