a96e0779bd5003c881cc679f9e91fe37392abb7f
Damian Mielczarek <mielczarek.damian99@o2.pl>
2023-12-13 15:20:30 +0100

    Removed old dashboard and added new one

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,26 +1,8 @@
-'use client'
-import PageTitle from "@/components/dashboardSide/PageTitle";
-import TableHover from "@/components/dashboardSide/TableHover";
-import { Card, CardBody, CardTitle, Col, Row } from "react-bootstrap";
-
-export default function Dashboard() {
+export default function Example() {
   return (
     <>
-      <PageTitle
-        heading="Regular Tables"
-        subheading="Tables are the backbone of almost all web applications."
-        icon="pe-7s-drawer icon-gradient bg-happy-itmeo"
-      />
-      <Row>
-        <Col lg="6">
-          <Card className="mb-3 main-card">
-            <CardBody>
-              <CardTitle>Moderatorzy i Administratorzy</CardTitle>
-              <TableHover />
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+
+
     </>
   );
 }
