8ce0e51e53d092b7f9a610cf8ac78f1946ae4889
Damian Mielczarek <mielczarek.damian99@o2.pl>
2023-12-22 18:55:52 +0100

    Bg login page changed

--- src/app/login/layout.jsx
+++ src/app/login/layout.jsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }) {
-  return <section className="w-full h-full">{children}</section>;
+  return <section className="w-full h-full bg-slate-200">{children}</section>;
 }
