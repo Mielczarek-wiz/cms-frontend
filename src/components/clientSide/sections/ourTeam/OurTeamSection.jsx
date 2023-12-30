bac2b39ba8d4965ed759cd9af4d6975cfe92be3a
Jey the Count <58235578+JakubOwadowski@users.noreply.github.com>
2023-12-30 15:37:17 +0100

    our team done

--- src/components/clientSide/sections/ourTeam/OurTeamSection.jsx
+++ src/components/clientSide/sections/ourTeam/OurTeamSection.jsx
@@ -2,27 +2,48 @@ import { ourTeamMock } from "./mock.js";
 import Image from "next/image.js";
 
 export default function OurTeamSection() {
-  const maxItems = 4;
+  const maxItems = 4; //pretty sure we need to replace that kek
   let index = 0;
   return (
-    <div className="flex mx-0 md:mx-32 py-4">
-      {ourTeamMock.map((item) => {
-        index += 1;
-        return index <= maxItems ? 
-        <div key={item.id} className="px-16 w-1/2">
-            <Image src={item.photo} width="170" height="172" alt="teammate" className="rounded flex align-middle"/>
-            <div className="ml-4 pr-4">
-                <p className="text-xl text-neutral-900">{item.name}</p>
+    <div className="bg-slate-100">
+      <p className="text-3xl text-neutral-900 flex text-center justify-center">
+        OUR TEAM
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 mx-0 md:mx-32 py-4">
+        {ourTeamMock.map((item) => {
+          index += 1;
+          return index <= maxItems ? (
+            <div key={item.id} className="p-8 flex items-start">
+              <Image
+                src={item.photo}
+                width="170"
+                height="172"
+                alt="teammate"
+                className="rounded flex align-middle"
+              />
+              <div className="ml-4 pr-4">
+                <a
+                  href="http://localhost:3000/"
+                  className="text-xl text-neutral-900 hover:text-blue-500 transition-all duration-500"
+                >
+                  {item.name}
+                </a>
                 <p className="text-neutral-900">{item.job}</p>
                 <div className="flex py-4">
-                    <Image src="phone.svg" alt="phone" width="20" height="20"/>
-                    <p className="pl-4">{item.phone}</p>
+                  <Image src="phone.svg" alt="phone" width="20" height="20" />
+                  <a
+                    href="tel:#"
+                    className="pl-4 text-neutral-900 hover:text-blue-500 transition-all duration-500"
+                  >
+                    {item.phone}
+                  </a>
                 </div>
                 <p className="text-neutral-600">{item.description}</p>
+              </div>
             </div>
-        </div> 
-        : null;
-      })}
+          ) : null;
+        })}
+      </div>
     </div>
   );
 }
