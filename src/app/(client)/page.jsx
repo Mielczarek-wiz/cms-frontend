d1efd85b633109c4b001403b2b5b6fca9d4dc9db
Jey the Count <58235578+JakubOwadowski@users.noreply.github.com>
2024-01-02 18:15:02 +0100

    Infoboxes section done

--- src/app/(client)/page.jsx
+++ src/app/(client)/page.jsx
@@ -1,12 +1,13 @@
 import BestOffersSection from "@/components/clientSide/sections/bestoffers/BestOffersSection";
 import CategoriesSection from "@/components/clientSide/sections/categories/CategoriesSection";
+import InfoboxesSection from "@/components/clientSide/sections/infoboxes/InfoboxesSection";
 import OurTeamSection from "@/components/clientSide/sections/ourTeam/OurTeamSection";
 import TestimonialsSection from "@/components/clientSide/sections/testimonials/TestimonialsSection";
 
 export default function Home() {
   return (
     <section className="w-full h-screen">
-    <BestOffersSection/>
+    <InfoboxesSection/>
     </section>
   )
 }
